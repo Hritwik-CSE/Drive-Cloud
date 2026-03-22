cbd5917129e9cbe02024ab8c6c3efaae7ebf639c
Hritwik Choudhury <hritwikchoudhury.cse@gmail.com>
2026-03-22 10:41:59 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ export const config = {
   googleDrive: {
     clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
     apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-    scopes: 'https://www.googleapis.com/auth/drive.readonly',
+    scopes: 'https://www.googleapis.com/auth/drive',
     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
     redirectUri: window.location.origin,
   },
